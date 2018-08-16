f505c134fbb76321a8849e4ccc8506b5a7d3c7c5
lizba <lizba@DESKTOP-9Q44OD2>
2018-08-15 23:23:33 -0500

    Add project files.

---
+++ VideoJuego/Enemigo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoJuego
+{
+    class Enemigo
+    {
+        //Auto properties
+        public string Nombre { get; set; }
+        public string PosicionX { get; set; }
+        public string PosicionY { get; set; }
+        public string Puntos { get; set; }
+        public string Mover { get; set; }
+        public string Disparar { get; set; }
+
+        //Properties
+        private string _explotar;
+        public string Explotar
+        {
+            get
+            {
+                return _explotar;
+            }
+            set
+            {
+                if (value.Length == 250)
+                //length sirve para obtener los numeros
+                {
+                    Console.WriteLine("Haz derrotado al villano");
+                }
+                else if (value.Length == 215)
+                //Poner la lada
+                {
+                    Console.WriteLine("Bonus de 14 puntos por tu rebeldía");
+                }
+                else
+                {
+                    Console.WriteLine("Sigue intentando con mas fuerza");
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+}
---
+++ VideoJuego/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoJuego
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemigo EnemigoUnico = new Enemigo();
+
+            EnemigoUnico.Nombre = "Hittler";
+            EnemigoUnico.PosicionX = "15";
+            EnemigoUnico.PosicionY = "8";
+            EnemigoUnico.Puntos = "250";
+            EnemigoUnico.Mover = "3";
+            EnemigoUnico.Disparar = "180";
+            EnemigoUnico.Explotar = "250";
+
+            Console.WriteLine("Nombre: " + EnemigoUnico.Nombre);
+            Console.WriteLine("Posicion en X: " + EnemigoUnico.PosicionX);
+            Console.WriteLine("Posicion en Y: " + EnemigoUnico.PosicionY);
+            Console.WriteLine("Puntos acumulados: " + EnemigoUnico.Puntos);
+            Console.WriteLine("Mover a la posicion: " + EnemigoUnico.Mover);
+            Console.WriteLine("Telefono Celular: " + EnemigoUnico.Disparar);
+            Console.WriteLine("Exploto: " + EnemigoUnico.Explotar);
+            Console.ReadLine();
+
+        }
+    }
+}
